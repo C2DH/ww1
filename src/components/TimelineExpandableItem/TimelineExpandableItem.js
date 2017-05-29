d3e20901744054ee78721f139bb9afa74e731cc9
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-05-29 15:45:15 +0200

    TimelineExpandableItem #10

--- src/components/TimelineExpandableItem/TimelineExpandableItem.js
+++ src/components/TimelineExpandableItem/TimelineExpandableItem.js
@@ -1,21 +1,60 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { Container, Row, Col } from 'reactstrap';
 import './TimelineExpandableItem.css'
 
-const TimelineExpandableItem = (props) => (
-  <Container className="TimelineExpandableItem__container">
-    <Row>
-      <Col md="2">
-        <p>{props.date}</p>
-      </Col>
-      <Col md="9" className="TimelineExpandableItem__title">
-        <h2>{props.title}</h2>
-      </Col>
-      <Col md="1">
-        <button className="TimelineExpandableItem__btn"><i className="icon-add" /></button>
-      </Col>
-    </Row>
-  </Container>
-)
+class TimelineExpandableItem extends PureComponent {
+
+    state = {
+      open:false
+    }
+
+    toggleExpand = () => {
+      this.setState({
+        open: !this.state.open
+      })
+    }
+
+render () {
+  const { item } = this.props
+  return (
+      <Container className={this.state.open ? "TimelineExpandableItem__containerOpen" : "TimelineExpandableItem__containerClose" }>
+        <Row>
+          <Col md="2">
+            <p>{this.props.item.date}</p>
+          </Col>
+          <Col md="9" className={this.state.open ? "TimelineExpandableItem__titleContainerOpen" : null}>
+            <h2 className="TimelineExpandableItem__title">{this.props.item.title}</h2>
+          </Col>
+          <Col md="1">
+            <button onClick={this.toggleExpand} className="TimelineExpandableItem__btn" key="button">
+              {this.state.open ? <i className="icon-remove" /> : <i className="icon-add" />}
+            </button>
+          </Col>
+        </Row>
+        {this.state.open ?
+        <div>
+          <Row>
+            <Col md="2" />
+            <Col md="9">
+              <p className="TimelineExpandableItem__text">{this.props.item.text}</p>
+            </Col>
+            <Col md="2" />
+          </Row>
+          <Row>
+            <Col md="2" />
+            <Col md="10">
+              <h6 className="TimelineExpandableItem__imgTitle">RELATED OBJECTS</h6>
+              <div>
+                <img className="img-responsives TimelineExpandableItem__img" src="http://placehold.it/200x120" />
+                <img className="img-responsives TimelineExpandableItem__img" src="http://placehold.it/200x120" />
+                <img className="img-responsives TimelineExpandableItem__img" src="http://placehold.it/200x120" />
+              </div>
+            </Col>
+          </Row>
+        </div> : null }
+      </Container>
+     )
+   }
+}
 
 export default TimelineExpandableItem
--- src/pages/Timeline/Timeline.js
+++ src/pages/Timeline/Timeline.js
@@ -3,6 +3,12 @@ import { Container, Row, Col } from 'reactstrap';
 import TimelineExpandableItem from '../../components/TimelineExpandableItem'
 import './Timeline.css'
 
+const items = [
+  { id:1,  date : "28 June 1914", title: "Assassination of Archduke Franz Ferdinand of Austria in Sarajevo", text: "Donec rutrum congue leo eget malesuada. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Cras ultricies ligula sed magna dictum porta. Pellentesque in ipsum id orci porta dapibus. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Curabitur aliquet quam id dui posuere blandit. Pellentesque in ipsum id orci porta dapibus. Donec sollicitudin molestie malesuada."},
+  { id:2,  date : "2 August 1914", title: "German Troops Invade Luxembourg", text: "Sed porttitor lectus nibh. Vivamus suscipit tortor eget felis porttitor volutpat. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim."},
+  { id:3,  date : "3 August 1914", title: "Germany Declares War to France", text: "Sed porttitor lectus nibh. Vivamus suscipit tortor eget felis porttitor volutpat. Praesent sapien massa, convallis a pellentesque nec, egestas non nisi. Nulla quis lorem ut libero malesuada feugiat. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Curabitur non nulla sit amet nisl tempus convallis quis ac lectus. Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim."},
+]
+
 const Timeline = () => (
   <div>
     <Row className="Timeline__TopRow">
@@ -15,10 +21,11 @@ const Timeline = () => (
         TimelineNav
       </Col>
       <Col md="10">
-        <TimelineExpandableItem date="28 June 1914" title="Assassination of Archduke Franz Ferdinand of Austria in Sarajevo" />
-        <TimelineExpandableItem date="2 August 1914" title="German Troops Invade Luxembourg" />
-        <TimelineExpandableItem date="28 June 1914" title="Assassination of Archduke Franz Ferdinand of Austria in Sarajevo" />
-        <TimelineExpandableItem date="28 June 1914" title="Assassination of Archduke Franz Ferdinand of Austria in Sarajevo" />
+        {
+          items.map(item => (
+              <TimelineExpandableItem item={item} key={item.id}/>
+          ))
+        }
       </Col>
     </Row>
   </div>
