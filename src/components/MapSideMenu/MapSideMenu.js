2a5de6be1f6a611c9f8d7e4a67ee52e44785134d
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-19 15:05:49 +0200

    styling map page

--- src/components/MapSideMenu/MapSideMenu.js
+++ src/components/MapSideMenu/MapSideMenu.js
@@ -1,27 +1,28 @@
 import React from 'react'
+import MapMenuItem from '../MapMenuItem'
 import './MapSideMenu.css'
 
 const MapSideMenu = () => (
   <div className="MapSideMenu__container">
     <div className="MapSideMenu__titleContainer">
-      <h5>TYPE</h5>
-      <p>Reset</p>
+      <h5 className="MapSideMenu__titleContainer_title">TYPE</h5>
+      <a className="MapSideMenu__titleContainer_reset">Reset</a>
     </div>
-    <ul className="MapSideMenu__sitesList">
-      <li>Bombed Site</li>
-      <li>Shelter</li>
-      <li>Industrial building</li>
-      <li>Cemetery</li>
-      <li>Hospital</li>
-      <li>Monument</li>
-      <li>Railway station</li>
-      <li>Administrative building</li>
-      <li>Army camp</li>
-      <li>Others</li>
-    </ul>
+    <div className="MapSideMenu__sitesList">
+      <MapMenuItem label="Bombed Site" iconClass="fa-bomb"/>
+      <MapMenuItem label="Shelter" iconClass="fa-home"/>
+      <MapMenuItem label="Industrial building" iconClass="fa-industry"/>
+      <MapMenuItem label="Cemetery" iconClass="fa-bomb"/>
+      <MapMenuItem label="Hospital" iconClass="fa-hospital-o"/>
+      <MapMenuItem label="Monument" iconClass="fa-university"/>
+      <MapMenuItem label="Railway station" iconClass="fa-train"/>
+      <MapMenuItem label="Administrative building" iconClass="fa-building"/>
+      <MapMenuItem label="Army camp" iconClass="fa-bomb"/>
+      <MapMenuItem label="Others" iconClass="fa-map-marker"/>
+    </div>
     <div className="MapSideMenu__titleContainer">
-      <h5>PERIOD</h5>
-      <p>Reset</p>
+      <h5 className="MapSideMenu__titleContainer_title">PERIOD</h5>
+      <a className="MapSideMenu__titleContainer_reset">Reset</a>
     </div>
     <div className="MapSideMenu__chartsContainer">
       chart
