93011b4be90a746252f8d9d9cd5130c791688866
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-19 11:46:26 +0200

    map in map page's layout

--- src/pages/MapPage/MapPage.js
+++ src/pages/MapPage/MapPage.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import ReactMapboxGl, { Layer, Marker, Feature, Cluster, ZoomControl } from 'react-mapbox-gl'
-import { Row, Col } from 'reactstrap'
+import { Container, Row, Col } from 'reactstrap'
 import MapSearchInput from '../../components/MapSearchInput'
 import MapSideMenu from '../../components/MapSideMenu'
 import './MapPage.css'
@@ -58,13 +58,14 @@ class MapPage extends PureComponent {
   render() {
     const { documents } = this.props
     return (
-      <div>
-        {/* {documents && <div>
-          {documents.map(doc => (
-            <pre key={doc.id}>{JSON.stringify(doc)}</pre>
-          ))}
-        </div>} */}
-        {/* <Row className="MapPage__TopRow">
+        //  {documents && <div>
+        //   {documents.map(doc => (
+        //     <pre key={doc.id}>{JSON.stringify(doc)}</pre>
+        //   ))}
+        // </div>}
+    <div>
+      <div className="MapPage__TopRow_container">
+        <Row className="MapPage__TopRow">
           <Col md="9" className="MapPage__TopInfo">
             <h2>Map</h2>
             <p><span>123/123</span> items shown</p>
@@ -73,38 +74,42 @@ class MapPage extends PureComponent {
             <MapSearchInput />
           </Col>
         </Row>
+      </div>
+
         <Row className="MapPage__MainRow">
-          <Col md="9">
-             map
+          <Col md="9" className="padding-right-0">
+            <div>
+              <ReactMapboxGl
+                style="mapbox://styles/mapbox/streets-v8"
+                center={[6.087, 49.667]}
+                zoom={[8]}
+                accessToken="pk.eyJ1IjoiYmlhbmNoaW1ybyIsImEiOiJOY0FqNUxrIn0.C2YPVWz8M0nPeG2pZLybKQ"
+                containerStyle={{
+                  height: "100vh",
+                  width: "100%",
+                  paddingtop: "100px"
+                }}>
+                <ZoomControl />
+                {documents && <Cluster ClusterMarkerFactory={this.clusterMarker} clusterThreshold={1} radius={60}>
+                {
+                  documents.map(doc =>
+                    <Marker
+                      key={doc.id}
+                      style={styles.marker}
+                      coordinates={doc.data.coordinates.geometry.coordinates.slice(0, 2).map(x => Number(x) + Math.random() / 1000).reverse()}>
+                      M
+                    </Marker>
+                  )
+                }
+              </Cluster>}
+               </ReactMapboxGl>
+            </div>
           </Col>
           <Col md="3" className="MapPage__SideMenu">
             <MapSideMenu />
           </Col>
-        </Row> */}
-        <ReactMapboxGl
-          style="mapbox://styles/mapbox/streets-v8"
-          center={[6.087, 49.667]}
-          zoom={[8]}
-          accessToken="pk.eyJ1IjoiYmlhbmNoaW1ybyIsImEiOiJOY0FqNUxrIn0.C2YPVWz8M0nPeG2pZLybKQ"
-          containerStyle={{
-            height: "100vh",
-            width: "100%"
-          }}>
-          <ZoomControl />
-          {documents && <Cluster ClusterMarkerFactory={this.clusterMarker} clusterThreshold={1} radius={60}>
-          {
-            documents.map(doc =>
-              <Marker
-                key={doc.id}
-                style={styles.marker}
-                coordinates={doc.data.coordinates.geometry.coordinates.slice(0, 2).map(x => Number(x) + Math.random() / 1000).reverse()}>
-                M
-              </Marker>
-            )
-          }
-        </Cluster>}
-         </ReactMapboxGl>
-      </div>
+        </Row>
+    </div>
     )
   }
 }
