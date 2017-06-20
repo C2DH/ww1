565eafa98ef83ff4945ecdf1ef22f59caf7c700e
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-20 16:30:52 +0200

    closes #20

--- src/pages/Themes/Themes.js
+++ src/pages/Themes/Themes.js
@@ -4,15 +4,25 @@ import ThemesCard from '../../components/ThemesCard'
 import './Themes.css'
 
 const Themes = () => (
-  <Container>
+  <Container fluid className="padding-r-l-0 Themes__container">
     <Row className="Themes__TitleRow">
       <h1>Themes</h1>
     </Row>
     <Row>
-      <ThemesCard title="the occupation" />
-      <ThemesCard title="grief and loss" />
-      <ThemesCard title="the food crisis" />
-      <ThemesCard title="migrations" />
+      {/* <ThemesCard title="the occupation" /> */}
+      <div className="Themes__theme_title_container">
+        <hr />
+        <h2 className="Themes__theme_title">The Occupation</h2>
+        <hr />
+        <h2 className="Themes__theme_title">Death on the home front</h2>
+        <hr />
+        <h2 className="Themes__theme_title">The food crisis</h2>
+        <hr />
+        <h2 className="Themes__theme_title">Death on the home front</h2>
+        <hr />
+        <h2 className="Themes__theme_title">The food crisis</h2>
+        <hr />
+      </div>
     </Row>
   </Container>
 )
