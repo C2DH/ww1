68b6758efcc61be6a7aa32a119d8724b27c7f8a5
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-20 14:46:05 +0200

    closes #19

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,37 +4,37 @@ import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = () => (
-  <Container>
+  <Container className="Home__container">
     <Row className="Home__MainRow">
-      <Col md="1" className="Home__MainRow-yearsCol"><h3>1914</h3></Col>
-      <Col md="10">
-        <h1>ÉISCHTE WELTKRICH</h1>
-        <h4>Remembering the Great War in Luxembourg</h4>
+      <Col md="1" sm="1" xs="1" className="Home__MainRow-yearsCol"><h3>1914</h3></Col>
+      <Col md="10" sm="10"  xs="10">
+        <h1 className="Home__title">ÉISCHTE WELTKRICH</h1>
+        <h4 className="Home__subtitle">Remembering the Great War in Luxembourg</h4>
       </Col>
-      <Col md="1" className="Home__MainRow-yearsCol"><h3>1918</h3></Col>
+      <Col md="1" sm="1"  xs="1" className="Home__MainRow-yearsCol"><h3>1918</h3></Col>
     </Row>
     <Row className="Home__NavigationRow">
-      <Col md="3" className="Home__NavigationCol-borderRight">
+      <Col md="3" sm="6" xs="6" className="Home__NavigationCol Home__NavigationCol_themes">
         <Link to="/themes">
-          <h6>DISCOVER THE</h6>
+          <h6 className="Home__NavigationCol_h6">DISCOVER THE</h6>
           <h2>Themes</h2>
         </Link>
       </Col>
-      <Col md="3" className="Home__NavigationCol-borderRight">
+      <Col md="3" sm="6" xs="6" className="Home__NavigationCol Home__NavigationCol_collection">
         <Link to="/collection">
-        <h6>BROWSE THE</h6>
+        <h6 className="Home__NavigationCol_h6">BROWSE THE</h6>
         <h2>Collection</h2>
         </Link>
       </Col>
-      <Col md="3" className="Home__NavigationCol-borderLeft">
+      <Col md="3" sm="6" xs="6" className="Home__NavigationCol Home__NavigationCol_map">
         <Link to="/map">
-          <h6>EXPLORE THE</h6>
+          <h6 className="Home__NavigationCol_h6">EXPLORE THE</h6>
           <h2>Map</h2>
         </Link>
       </Col>
-      <Col md="3" className="Home__NavigationCol-borderLeft">
+      <Col md="3" sm="6" xs="6" className="Home__NavigationCol Home__NavigationCol_timeline">
         <Link to="/timeline">
-          <h6>FOLLOW THE</h6>
+          <h6 className="Home__NavigationCol_h6">FOLLOW THE</h6>
           <h2>Timeline</h2>
         </Link>
       </Col>
