8a5118b7f34325f992f4269272f9dd8509827561
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-23 17:46:30 +0200

    working on #25

--- src/components/CollectionDoc/CollectionDoc.js
+++ src/components/CollectionDoc/CollectionDoc.js
@@ -8,7 +8,10 @@ const CollectionDoc = ({ doc, hasImage }) => (
   <div className="CollectionDoc">
     <Link to={{ pathname:`/collection/item/${doc.id}`, state:{modal:true} }} >
       { hasImage && ( <img src={doc.snapshot} alt={doc.title} style={{height:'100%', width:'100%'}}/> )}
-      { !hasImage && ( <div style={{height:200}}>{doc.title} ({doc.data.type})</div> )}
+      { !hasImage && (
+        <div className={`CollectionDoc__inner_container ${(doc.data.type === 'report') && 'CollectionDoc__inner_container_audio'}`}>
+          {doc.title} ({doc.data.type})
+        </div> )}
       {/* {get(doc, 'data.coordinates.geometry.coordinates')} */}
     </Link>
   </div>
