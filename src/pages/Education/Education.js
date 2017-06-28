f4f2be189117347135c67ea7e3d3492302199301
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-28 17:18:00 +0200

    styling template

--- src/pages/Education/Education.js
+++ src/pages/Education/Education.js
@@ -9,7 +9,7 @@ const Education = () => (
   <Container fluid className="padding-r-l-0">
     <BigTitle title="Education" />
     <div className="Education__whiteBackground">
-      <Row>
+      <Row className="Education__TwoColRow">
         <Col md="6" className="Education__leftCol">
           <h6>Introduction and pedagogical approach</h6>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quis lorem ut libero malesuada feugiat. Vivamus magna justo, lacinia eget consectetur sed, convallis at tellus. Pellentesque in ipsum id orci porta dapibus. Quisque velit nisi, pretium ut lacinia in, elementum id enim. Vestibulum ac diam sit amet quam vehicula elementum sed sit amet dui. Nulla porttitor accumsan tincidunt. Donec sollicitudin molestie malesuada.</p>
@@ -25,10 +25,12 @@ const Education = () => (
         <Col md="12">
           <h6>Pedagogical manuals</h6>
           <div className="Education__Card-container">
-            <ManualCard />
-            <ManualCard />
-            <ManualCard />
-            <ManualCard />
+            <div style={{overflow: 'auto'}}>
+              <ManualCard />
+              <ManualCard />
+              <ManualCard />
+              <ManualCard />
+            </div>
           </div>
         </Col>
       </Row>
