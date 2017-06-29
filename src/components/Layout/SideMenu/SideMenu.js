a97a891a2710295058d33c52da95fa728ecd0fc3
lorenzobianchi <lorenzo.bianchi@inmagik.com>
2017-06-29 15:06:58 +0200

    styling sidememu

--- src/components/Layout/SideMenu/SideMenu.js
+++ src/components/Layout/SideMenu/SideMenu.js
@@ -29,6 +29,9 @@ class SideMenu extends PureComponent {
        <button className="SideMenu__menuBtn" onClick={this.toggleMenu} key="button">
          {this.state.open ? <div><i className="icon-close" /><p className="SideMenu__menuBtn-text">close</p></div> : <div><i className="icon-dehaze SideMenu__icon" /><p className="SideMenu__menuBtn-text">Menu</p></div>}
        </button>
+       <div className="SideMenu__vertical_title_container">
+         <p className="SideMenu__vertical_title">ÉISCHTE WELTKRICH</p>
+       </div>
      </div>
      {this.state.open ? <OpenSideMenu key="close" closeMenu={this.toggleMenu} key="opensidemenu"/> : null}
     </CSSTransitionGroup>
