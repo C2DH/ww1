a2e49dd789177e9ffb31e1c56f8eb65ddfd775c1
gffuma <fumagalli.gf@gmail.com>
2017-07-21 16:05:40 +0200

    Working on module object

--- src/pages/Module/ModuleObject.js
+++ src/pages/Module/ModuleObject.js
@@ -1,81 +1,89 @@
 import React, { PureComponent } from 'react'
+import { pure } from 'recompose'
 import { connect } from 'react-redux'
 import { get } from 'lodash'
 import { Container, Row, Col } from 'reactstrap';
 import { Card, CardImg, CardBlock } from 'reactstrap';
 
 import Background from '../../components/Background'
-import './Module.css'
 
 const fullHeight = { height: '100%'}
+
+const ModuleObjectCard = pure(({ module }) => {
+  const cover = get(module, 'id.attachment')
+  const size = get(module, 'size', 'medium')
+
+  let cardImgHeight = '400px'
+  if (size === 'small') {
+    cardImgHeight = '250px'
+  } else if(size === 'medium') {
+    cardImgHeight = '400px'
+  } else if (size === 'big') {
+    cardImgHeight = '90vh'
+  }
+
+  const objectStyle = {
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+  }
+
+  const objectContainerStyle = {
+    width: '100%',
+    height: cardImgHeight,
+    backgroundSize: 'cover',
+    backgroundImage: `url(${cover})`
+  }
+
+  return (
+    <div style={objectStyle} className="Module__container">
+      <Card className="Module__objectCard">
+        <div style={objectContainerStyle}></div>
+        <CardBlock>
+          <div className="d-inline-flex">
+            <i className="icon-hand Mods__DocumentOnly_Card_icon"  />
+            <div className="Module__objectCard_text">
+              {module.caption}
+            </div>
+          </div>
+        </CardBlock>
+      </Card>
+    </div>
+  )
+})
 
 class ModuleObject extends PureComponent {
   render() {
     const { chapter, module } = this.props
-    const cover = 'https://images.pexels.com/photos/416676/pexels-photo-416676.jpeg?h=350&auto=compress&cs=tinysrgb'
-    const size = get(module, 'size', 'medium')
 
-    let cardImgHeight = '400px'
-    if(size === 'small'){
-      cardImgHeight = '250px'
-    }
-    if(size === 'medium'){
-      cardImgHeight = '400px'
-    }
-    if(size === 'big'){
-      cardImgHeight = '90vh'
-    }
+    const size = get(module, 'size')
 
-    const objectStyle = {
-      backgroundColor: get(module, 'background.color', '#fff'),
-      alignItems: 'center',
-    }
-
-    const objectContainerStyle = {
-      width: '100%',
-      height: cardImgHeight,
-      backgroundSize: 'cover',
-      backgroundImage: `url(${cover})`
-    }
-
-    const ModuleObjectCard = () => (
-      <div style={objectStyle} className="Module__container">
-        <Card className="Module__objectCard">
-          <div style={objectContainerStyle}></div>
-          <CardBlock>
-            <div className="d-inline-flex">
-              <i className="icon-hand Mods__DocumentOnly_Card_icon"  />
-              <div className="Module__objectCard_text">
-                {module.caption}
-              </div>
-            </div>
-          </CardBlock>
-        </Card>
-      </div>
-
-    )
+    const backgroundColor = get(module, 'background.color')
+    const backgroundImage = get(module, 'background.object.id.attachment')
+    const backgroundOverlay = get(module, 'background.object.overlay')
 
     return (
-      <div style={{height:'100%'}}>
-        <Background color={get(module, 'background.color')} />
+      <div style={fullHeight}>
+        <Background image={backgroundImage} color={backgroundColor} overlay={backgroundOverlay} />
+
           {(size === 'small') &&
             <Row style={fullHeight}>
               <Col md="4" />
               <Col md="4" style={fullHeight}>
-                <ModuleObjectCard />
+                <ModuleObjectCard module={module} />
               </Col>
               <Col md="4" />
           </Row>}
+
           {(size === 'medium') &&
             <Row style={fullHeight}>
               <Col md="2" />
               <Col md="8">
-                <ModuleObjectCard />
+                <ModuleObjectCard module={module} />
               </Col>
               <Col md="2" />
           </Row>}
 
-         {(size === 'big') && <ModuleObjectCard />}
+         {(size === 'big') && <ModuleObjectCard module={module} />}
       </div>
     )
   }
