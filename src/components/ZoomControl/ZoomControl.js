112c629ee96c5fff29e8fcbec7a17f27f92d8200
gffuma <fumagalli.gf@gmail.com>
2017-07-24 11:46:40 +0200

    Better zoom control

--- src/components/ZoomControl/ZoomControl.js
+++ src/components/ZoomControl/ZoomControl.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './ZoomControl.css'
 
+const fixFloat = f => +(f.toFixed(2))
+const INC = 0.2
+
 const ZoomControl = ({zoom, maxZoom, minZoom, zoomTo}) => (
   <div className="ZoomControl__container">
-    <button className="" onClick={zoomTo(zoom+0.2)}><i className="icon-zoom_in" /></button>
-    <button className="" onClick={zoomTo(0.0)}>Reset</button>
-    <button className="ZoomControl__minus_btn" onClick={zoomTo(zoom-0.2)}><i className="icon-zoom_out" /></button>
+    <button className="" disabled={fixFloat(zoom + INC) > 3} onClick={() => zoomTo(fixFloat(zoom + INC))}><i className="icon-zoom_in" /></button>
+    <button className="" onClick={() => zoomTo(0.5)}>Reset</button>
+    <button disabled={fixFloat(zoom - INC) < -1} className="ZoomControl__minus_btn" onClick={() => zoomTo(fixFloat(zoom - INC))}>
+      <i className="icon-zoom_out" />
+    </button>
   </div>
 )
 
