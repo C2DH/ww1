07ce395ffe4d7059b2528ae2c52b229180cdcc61
gffuma <fumagalli.gf@gmail.com>
2017-08-02 17:48:22 +0200

    Working on improving timeline scroll

--- src/components/TimelineExpandableYear/TimelineExpandableYear.js
+++ src/components/TimelineExpandableYear/TimelineExpandableYear.js
@@ -3,17 +3,22 @@ import './TimelineExpandableYear.css'
 
 const months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-const Month = ({ month, open, disabled }) => {
+const Month = ({ month, open, disabled, onMonthClick }) => {
   if(open) {
     return (
       <div className="d-inline-block">
         <i className="icon-hand TimelineExpandableYear__month_marker_hand" />
         <i className="icon-fiber_manual_record TimelineExpandableYear__month_marker_active" />
-      </div>)}
+      </div>)
+    }
   if(disabled) {
     return (<i className="icon-fiber_manual_record TimelineExpandableYear__month_marker" style={{color: 'green'}}/>)
   } else {
-    return (<i className="icon-fiber_manual_record TimelineExpandableYear__month_marker" />)
+    return (<i className="icon-fiber_manual_record TimelineExpandableYear__month_marker" onClick={e => {
+      e.preventDefault()
+      e.stopPropagation()
+      onMonthClick(month)
+    }} />)
   }
 }
 
@@ -21,6 +26,10 @@ class TimelineExpandableYear extends PureComponent {
 
   onYearClick = () => {
     this.props.onYearClick(this.props.year)
+  }
+
+  onMonthClick = (month) => {
+    this.props.onYearClick(this.props.year, month)
   }
 
    render() {
@@ -31,7 +40,13 @@ class TimelineExpandableYear extends PureComponent {
         {this.props.open ?
           <div className="TimelineExpandableYear__expandable hidden-md-down">
             {months.map(month =>(
-              <Month key={month} open={month === openMonth} disabled={disabledMonths.indexOf(month) !== -1}/>
+              <Month
+                key={month}
+                month={month}
+                open={month === openMonth}
+                onMonthClick={this.onMonthClick}
+                disabled={disabledMonths.indexOf(month) !== -1}
+              />
             ))}
           </div>
          : null}
