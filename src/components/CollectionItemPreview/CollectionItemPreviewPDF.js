5598f4fec15f0248c02269cc47113e4af59607b5
Mauro Bianchi <bianchimro@gmail.com>
2017-08-07 11:48:29 +0200

    new pdf viewer. closes #33

--- src/components/CollectionItemPreview/CollectionItemPreviewPDF.js
+++ src/components/CollectionItemPreview/CollectionItemPreviewPDF.js
@@ -1,46 +1,49 @@
 import React from 'react';
 import ReactPDF from 'react-pdf';
+import PDF from 'react-pdf-js';
 import './CollectionItemPreview.css'
+
 
 export default class CollectionItemPreviewPDF extends React.PureComponent {
 
   state = {
     total : null,
-    pageIndex: null,
-    pageNumber: null,
-    scale: 1.0,
+    pages: 0,
+    page: undefined,
+    scale: 1,
   }
 
 
-  onDocumentLoad = ({ total }) => {
-    this.setState({ total });
+  onDocumentComplete = (pages) => {
+    console.log(1, pages)
+    this.setState({ page:1, pages });
   }
 
-  onPageLoad = ({ pageIndex, pageNumber }) => {
-    this.setState({ pageIndex, pageNumber });
+  onPageComplete = (page) => {
+    this.setState({ page });
   }
 
   nextPage = () => {
-    if(this.state.pageIndex < this.state.total - 1){
-      this.setState({pageIndex:this.state.pageIndex + 1})
+    if(this.state.page < this.state.pages - 1){
+      this.setState({page:this.state.page + 1})
     }
   }
 
   prevPage = () => {
-    if(this.state.pageIndex > 0){
-      this.setState({pageIndex:this.state.pageIndex - 1})
+    if(this.state.page > 1){
+      this.setState({page:this.state.page - 1})
     }
   }
 
   zoomIn = () => {
-    if(this.state.scale < 16){
-      this.setState({scale:this.state.scale * 2})
+    if(this.state.scale < 2){
+      this.setState({scale:this.state.scale + 0.2 })
     }
   }
 
   zoomOut = () => {
     if(this.state.scale > 1){
-      this.setState({scale:this.state.scale / 2})
+      this.setState({scale:this.state.scale - 0.2})
     }
   }
 
@@ -49,26 +52,45 @@ export default class CollectionItemPreviewPDF extends React.PureComponent {
     return (
     <div>
       <div className="CollectionItemPreview__doc_preview">
-        <ReactPDF
-          scale={this.state.scale}
-          pageIndex={this.state.pageIndex}
-          onDocumentLoad={this.onDocumentLoad}
-          onPageLoad={this.onPageLoad}
-          // file={`https://cors-anywhere.herokuapp.com/${doc.src}`}
-          file={`${doc.src}`}
-        />
+        <div className="CollectionItemPreview__pdf_preview">
+
+          <PDF
+            file={`${doc.src}`}
+            onDocumentComplete={this.onDocumentComplete}
+            onPageComplete={this.onPageComplete}
+            page={this.state.page}
+            // ref={(ref)=>{this.pdf=ref}}
+            scale={this.state.scale}
+          />
+
+        </div>
       </div>
       <div className="CollectionItem__doc_controls">
 
         <div className="CollectionItemPreviewPDF__controls">
-          <div style={{padding:8}}>Page <span className="CollectionItemPreviewPDF__controls_current_page">{this.state.pageNumber}</span> of {this.state.total}</div>
-          <div className="CollectionItemPreviewPDF__controls_page_arrows"><button onClick={(evt)=>{this.prevPage()}}><i className="material-icons">arrow_downward</i></button><span>{' | '}</span><button  onClick={(evt)=>{this.nextPage()}}><i className="material-icons">arrow_upward</i></button></div>
-          <div className="CollectionItemPreviewPDF__controls_zoom"><button onClick={(evt)=>{this.zoomIn()}}><i className="icon-zoom_in" /></button><span>{' | '}</span><button  onClick={(evt)=>{this.zoomOut()}}><i className="icon-zoom_out" /></button></div>
-          <div className="CollectionItemPreviewPDF__controls_fit_to_page">
+          <div style={{padding:8}}>Page <span className="CollectionItemPreviewPDF__controls_current_page">{this.state.page}</span> of {this.state.pages}</div>
+          <div className="CollectionItemPreviewPDF__controls_page_arrows">
+            <button onClick={(evt)=>{this.prevPage()}}>
+              <i className="material-icons pointer">arrow_downward</i>
+            </button><span>{' | '}</span>
+            <button  onClick={(evt)=>{this.nextPage()}}>
+              <i className="material-icons pointer">arrow_upward</i>
+            </button>
+          </div>
+          <div className="CollectionItemPreviewPDF__controls_zoom">
+            <button onClick={(evt)=>{this.zoomIn()}}>
+              <i className="icon-zoom_in pointer" />
+            </button><span>{' | '}</span>
+            <button  onClick={(evt)=>{this.zoomOut()}}>
+              <i className="icon-zoom_out pointer" />
+            </button>
+          </div>
+          {/* fit to page is not easy to implement, given existing pdf modules */}
+          {/* <div className="CollectionItemPreviewPDF__controls_fit_to_page">
             <select>
               <option>Fit to page</option>
             </select>
-          </div>
+          </div> */}
           <button className="CollectionItemPreviewPDF__download_btn"><i className="icon-file_download" /></button>
         </div>
       </div>
