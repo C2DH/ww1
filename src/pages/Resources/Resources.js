0b71f33e0710275e30413914718d3463dd226fc8
gffuma <fumagalli.gf@gmail.com>
2017-08-07 15:43:47 +0200

    Fixed typo

--- src/pages/Resources/Resources.js
+++ src/pages/Resources/Resources.js
@@ -19,7 +19,7 @@ class Resources extends PureComponent {
   }
 
   componentWillUnmount() {
-    this.props.unloadCollectionDocuments()
+    this.props.unloadResourceDocuments()
   }
 
   render() {
