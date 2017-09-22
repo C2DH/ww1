392d4fc861b81f2e9f244e0ef53bf1dfead1c76f
Mauro Bianchi <bianchimro@gmail.com>
2017-09-22 16:30:28 +0200

    scroll module switch experiment

--- src/pages/Module/Module.js
+++ src/pages/Module/Module.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import ReactDOM from 'react-dom'
 import { connect } from 'react-redux'
 import { Container } from 'reactstrap'
 import ModuleText from './ModuleText'
@@ -7,10 +8,17 @@ import ModuleTextObject from './ModuleTextObject'
 import ModuleGallery from './ModuleGallery'
 import ModuleMap from './ModuleMap'
 import ModuleMapText from './ModuleMapText'
+import WayPoint from 'react-waypoint'
+
+import { get } from 'lodash'
+import { setScrollDelta } from '../../state/actions'
 import './Module.css'
 
 import {
+  getTheme,
   getChapter,
+  getTotalChapterModules,
+  getChapterIndex,
   makeGetModule,
 } from '../../state/selectors'
 
@@ -72,13 +80,116 @@ const fakeModule = {
   }
 }
 
+
+const BASE_SCROLL_HELPER_HEIGHT = 1000
+
+const scrollHelperMapStateToProps = (state) => ({
+    scroll: state.scroll,
+})
+
+
+const ScrollHelperTop = connect(scrollHelperMapStateToProps) (class extends React.PureComponent {
+
+
+  render(){
+    return (
+        <div style={{
+            height:this.props.scroll, backgroundColor:'yellow', width: 1000, position:'relative', opacity: 0.2,
+
+            // paddingTop: this.state.delta,
+            // marginBottom: this.state.delta/2,
+            // top: this.state.margin,
+            // marginBottom: this.state.margin
+          }}>
+          <div ref={(r)=>{
+            this.bottomHook=r;
+          }} style={{position:'absolute', top:0, height:2, backgroundColor:'red', right:0, left:0}}></div>
+        </div>
+    )
+
+  }
+})
+
+
+
+const ScrollHelperBottom = connect(scrollHelperMapStateToProps, { setScrollDelta }) (class extends React.PureComponent {
+
+  bottomHook = null;
+
+  handleScroll = (e) => {
+    var rect = this.bottomHook.getBoundingClientRect();
+    const delta = this.initialTop - rect.top
+    console.log("delta", delta, this.props.moduleIndex)
+    this.props.setScrollDelta(delta)
+
+  }
+
+  componentDidMount(){
+    var rect = this.bottomHook.getBoundingClientRect();
+    this.initialTop = rect.top
+    window.addEventListener('scroll', this.handleScroll, false)
+  }
+
+  componentWillUnmount(){
+    window.removeEventListener('scroll', this.handleScroll, false)
+  }
+
+  render(){
+    return (
+        <div style={{
+            height:BASE_SCROLL_HELPER_HEIGHT-this.props.scroll, backgroundColor:'teal', width: '100%', position:'relative',
+            opacity: 0.05,
+          }}>
+          <div ref={(r)=>{
+            this.bottomHook=r;
+          }} style={{position:'absolute', bottom:0, height:2, backgroundColor:'red', right:0, left:0}}></div>
+        </div>
+    )
+
+  }
+})
+
 class Module extends PureComponent {
+
+  componentWillReceiveProps (nextProps){
+    console.log(nextProps.scroll)
+    if (this.props.scroll !== nextProps.scroll){
+      if(nextProps.scroll > 800){
+        console.log("bye bye")
+        this.toNextModule()
+        window.scrollTo(0,0)
+      }
+    }
+  }
+
+  componentDidMount(){
+    this.props.setScrollDelta(0)
+  }
+
+  toNextModule = () => {
+    const { moduleIndex, totalChapterModules, history, theme, chapter, chapterIndex } = this.props
+    const nextChapterSlug = get(theme, `stories[${Number(chapterIndex) + 1}].slug`)
+    const themeUrl = `/themes/${theme.slug}`
+    const chapterUrl = `${themeUrl}/chapters/${chapter.slug}`
+
+    if (moduleIndex < totalChapterModules) {
+      history.push(`${chapterUrl}/modules/${Number(moduleIndex) + 1}`)
+    } else {
+      // Go to cover of next chapter
+      history.push(`${themeUrl}/chapters/${nextChapterSlug}`)
+    }
+  }
+
+
+
   render() {
-    const { chapter, module } = this.props
+    const { chapter, module, moduleIndex } = this.props
     if (!module) {
       return null
     }
-    return <div style={moduleContainerStyle}>
+    return  <div>
+    <ScrollHelperTop moduleIndex={moduleIndex}/>
+    <div style={{ ...moduleContainerStyle, opacity:1 - (this.props.scroll/1000) }}>
       {React.createElement(getModuleComponent(module.module), {
         chapter,
         module,
@@ -90,6 +201,8 @@ class Module extends PureComponent {
       {/* <ModuleMap chapter={chapter} module={fakeModule.object} /> */}
       {/* <ModuleMapText chapter={chapter} module={fakeModule.text_object}  /> */}
     </div>
+    <ScrollHelperBottom moduleIndex={moduleIndex}/>
+  </div>
   }
 }
 
@@ -99,11 +212,16 @@ const makeMapStateToProps = () => {
   const mapStateToProps = (state, props) => {
     const moduleIndex = props.match.params.moduleIndex
     return {
+      theme: getTheme(state),
       chapter: getChapter(state),
       module: getModule(state, moduleIndex),
+      scroll: state.scroll,
+      moduleIndex: moduleIndex,
+      chapterIndex: getChapterIndex(state),
+      totalChapterModules: getTotalChapterModules(state),
     }
   }
   return mapStateToProps
 }
 
-export default connect(makeMapStateToProps)(Module)
+export default connect(makeMapStateToProps, { setScrollDelta })(Module)
