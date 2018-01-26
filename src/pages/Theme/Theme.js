1e7895d3f79d99e0eac71e0bf70f18273a1fe1d7
uf0 <scnepa222@gmail.com>
2018-01-26 16:45:17 +0100

    styling theme intro

--- src/pages/Theme/Theme.js
+++ src/pages/Theme/Theme.js
@@ -45,54 +45,37 @@ class Theme extends PureComponent {
     const containerStyle = { backgroundImage: `url(${getThemeCover(theme)})` }
 
     return (
-      <div className="Theme__wrapper">
-
+      <div className="Theme__wrapper animated fadeIn">
       <Background
         image={get(theme, 'covers[0].attachment')}
         bbox={get(theme, 'data.background.bbox', [])}
         overlay={get(theme, 'data.background.overlay')}
         color={get(theme, 'data.background.backgroundColor')}/>
-      <Container className="Theme__container">
-        <div className="Theme__info_wrapper">
+
+      <div className="Theme__title d-md-none d-flex align-items-center justify-content-center">
+        <h2 className="m-0">{this.context.t('theme')}</h2>
+      </div>
+
+      <Container className="Theme__container d-flex align-items-end align-items-md-center text-center text-md-left">
+        <div className="Theme__info_wrapper my-4">
           <Row>
             <Col>
-              <h6>THEME</h6>
-              <h1>{theme.translated.title}</h1>
-              <div className="Theme__text_container">
-                <p>{theme.translated.abstract}</p>
-              </div>
-              {firstChapterSlug && <button onClick={this.jumpToFirstChapter} className="Theme__start_btn">START</button>}
+              <h6 className="animated fadeIn d-none d-md-block">{this.context.t('theme')}</h6>
+              <h1 className="animated fadeIn">{theme.translated.title}</h1>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="7">
+              <p className="animated fadeIn mb-5 mb-md-4">{theme.translated.abstract}</p>
+              {firstChapterSlug && <a onClick={this.jumpToFirstChapter} role="button" className="btn btn-secondary Theme__start_btn animated fadeIn AtlasGrotesk-Medium-Web">{this.context.t('start')}</a>}
             </Col>
           </Row>
         </div>
       </Container>
       <div className="Theme__chapters_btn_container">
-        <h6>Chapters</h6>
+        <h6 className="d-none d-md-block">Chapters</h6>
         <button className="Theme__chapters_btn" onClick={this.toggleChapters}><i className="material-icons md-24">{this.state.open ? 'close' : 'list'}</i></button>
       </div>
-          {/*<div className="Theme__chapters_container">
-            <div className="Theme__chapters__inner_container">
-              <div className="Theme__chapters_top">
-                <button className="btn btn-secondary Theme__chapters_back_btn AtlasGrotesk-Medium-Web">
-                  {theme.translated.title}
-                </button>
-              </div>
-                <div className="Theme__chapter_scroll_container">
-                  {get(theme, 'stories').map((chapter, i) => (
-                    <div key={chapter.id} className="Theme__chapter">
-                      <div className="Theme__chapter_title">
-                        <Link to={`/themes/${theme.slug}/chapters/${chapter.slug}`}><h3>{chapter.translated.title}</h3></Link>
-                      </div>
-                      <div className="Theme__chapter_num">
-                        <div>
-                          <h2>{i + 1}</h2>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-              </div>
-            </div>
-          </div>*/}
       <CSSTransitionGroup component="div"
         transitionName="chapter"
         transitionEnterTimeout={500}
@@ -106,6 +89,9 @@ class Theme extends PureComponent {
   }
 }
 
+Theme.contextTypes = {
+  t: React.PropTypes.func.isRequired
+}
 
 const mapStateToProps = state => ({
   makeUrl: getMakeLangUrl(state),
