95cfbfd3a86f6cd1b375bb04f95245f59f104ada
uf0 <scnepa222@gmail.com>
2018-02-07 18:15:04 +0100

    close #64

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,8 +3,10 @@ import './NotFound.css'
 
 const NotFound = () => (
   <div className='NotFound'>
-    <h1 className='NotFound__404'>4<span style={{ color: 'white' }}>0</span>4</h1>
-    <h1>Page Not Found</h1>
+    <div className="NotFound_cont">
+      <h1 className='NotFound__404'>ops</h1>
+      <h3>This page doesn't exist</h3>
+    </div>
   </div>
 )
 
