61db0dcbe67ba24af09fd2443dfa84c88995586a
uf0 <scnepa222@gmail.com>
2018-02-09 10:40:58 +0100

    terms of use page

--- src/pages/TermsOfUse/TermsOfUse.js
+++ src/pages/TermsOfUse/TermsOfUse.js
@@ -1,21 +1,29 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import {  Container, Row, Col } from 'reactstrap'
 import StaticStory from '../../components/StaticStory'
 import BigTitle from '../../components/BigTitle'
 import './TermsOfUse.css'
 
-const TermOfUse = () => (
-    <div className="About__wrapper">
-      <Container>
-        <BigTitle title="Terms of use" />
-        <Row>
-          <Col>
-              <StaticStory slug='terms-of-use' />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+class TermOfUse extends PureComponent {
 
-)
+  render(){
+        return(
+          <div className="About__wrapper">
+            <Container>
+              <BigTitle title={this.context.t('terms of use')} />
+              <Row>
+                <Col xs="12" md="8">
+                    <StaticStory slug='terms-of-use' />
+                </Col>
+              </Row>
+            </Container>
+          </div>
+        )
+  }
+}
+
+TermOfUse.contextTypes = {
+  t: React.PropTypes.func.isRequired
+}
 
 export default TermOfUse
