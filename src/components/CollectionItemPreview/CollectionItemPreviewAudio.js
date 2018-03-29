09898de110f6494b9ae65184c8eec502b7887b1b
uf0 <scnepa222@gmail.com>
2018-03-29 14:58:14 +0200

    fix audio in collection on Safari

--- src/components/CollectionItemPreview/CollectionItemPreviewAudio.js
+++ src/components/CollectionItemPreview/CollectionItemPreviewAudio.js
@@ -2,22 +2,31 @@ import React from 'react';
 import JSONTree from 'react-json-tree'
 import AudioPlayer from '../AudioPlayer'
 import CollectionItemDownload from '../CollectionItemDownload'
+import { isSafari, replaceExtension } from '../../utils'
+import { get } from 'lodash'
 
 export default class CollectionItemPreviewAudio extends React.PureComponent {
 
   render() {
     const { doc } = this.props
-    // #TODO: FIXME SERVERS SIDE (OR HANDLE WITH PROXY)
-    let attachment = doc.attachment
-    if (attachment.indexOf("http://178.62.220.183/media/http") == 0){
-      attachment = decodeURIComponent(attachment.replace("http://178.62.220.183/media/", ""))
+
+    let media = get(doc, 'attachment')
+    const sources = get(doc, 'data.sources', [])
+    const title = get(doc, 'translated.title')
+    if (isSafari()) {
+      const sourceAlternative = sources.filter(s => s.type === 'audio/mpeg')
+      if (sourceAlternative.length > 0) {
+        media = sourceAlternative[0].src
+      } else {
+        media = replaceExtension(media, 'mp3')
+      }
     }
+    if(!media){ return null }
 
     return (
       <div className="CollectionItemPreview__doc_preview">
-        {/* <JSONTree data={doc} /> */}
         <div className="CollectionItemPreview__audio_wrapper">
-            <AudioPlayer source={`${attachment}`} title={doc.title}/>
+            <AudioPlayer source={`${media}`} title={title}/>
         </div>
       <div className="CollectionItem__doc_controls">
         <CollectionItemDownload doc={doc} className="CollectionItem__btn_download"/>
