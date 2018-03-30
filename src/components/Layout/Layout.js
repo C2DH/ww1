12840fd4ee805317d324aa2e313eed39bfed8b04
uf0 <scnepa222@gmail.com>
2018-03-30 11:26:23 +0200

    added translation, styled and read more link #169

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -21,17 +21,25 @@ class Layout extends PureComponent {
           </div>
           {scrollLock && <ScrollLock touchScrollTarget={this.scrollTarget} />}
           <CookieBanner
-            message="We use cookies"
+            message={this.context.t('cookies')}
+            buttonMessage={this.context.t('got it')}
+            link={{msg:this.context.t('read more'), url:'https://ww1.lu', target:'_blank'}}
             onAccept={() => {}}
             styles={{
-              banner:{position:'fixed', bottom:0, zIndex:9999999,backgroundColor: 'rgba(245, 99, 80, 0.6)'},
-              message: { fontWeight: 400 }
+              banner:{position:'fixed', top:0, zIndex:9999999,backgroundColor: 'rgba(245, 99, 80, 0.6)'},
+              message: { fontWeight: 400 },
+              button: { fontFamily: "'Atlas Grotesk Web', Arial, sans-serif",backgroundColor: 'white', borderRadius: 999, top:0,marginTop:6,padding:'4px 12px' },
             }}
             dismissOnScroll={true} />
         </div>
       )
     }
 }
+
+Layout.contextTypes = {
+  t: React.PropTypes.func.isRequired
+}
+
 
 const mapStateToProps = state => ({
   scrollLock: state.scrollLock,
